0b2522c904b78de8dbf7268b3e80c36e7c39703b
Alejo <alejobuc@gmail.com>
2023-05-15 15:32:42 -0300

    beta dos jugadores V:0.1

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,64 @@ import Footer from "./components/footer.jsx"
 
 function App() {
   const [game, setGame] = useState(0)
+  const [player, setPlayer] = useState(0)
 
   const startGame = () => {
     if (game == 0) {
       return (
+        <>
+          <div className="menu">
+            <h1 className="title">Memory Game</h1>
+            <div className="button-row">
+              <button onClick={() => handleState(6)} className="button"> 2 X 3</button>
+              <button onClick={() => handleState(12)} className="button"> 3 X 4</button>
+            </div>
+            <div className="button-row" style={{marginBottom: "30px"}}>
+              <button onClick={() => handleState(20)} className="button"> 4 X 5</button>
+              <button onClick={() => handleState(30)} className="button"> 5 X 6</button>
+            </div>
+            <div onClick={() => setPlayer(0)} className='back-home'>
+              <button className='btn-back' onClick={() => handleState(0)}>
+                Back
+              </button>
+            </div>
+          </div>
+        </>
+
+      )
+    }
+    return (
+      <div className='flex-menu'>
+        <Game type={game} handleState={handleState} handlePlayer={handlePlayer} player={player}/>
+      </div>
+    )
+  }
+
+  const numberOfPlayers = () => {
+    if (player == 0) {
+      return (
         <div className="menu">
           <h1 className="title">Memory Game</h1>
           <div className="button-row">
-            <button onClick={() => handleState(6)} className="button"> 2 X 3</button>
-            <button onClick={() => handleState(12)} className="button"> 3 X 4</button>
+            <button onClick={() => handlePlayer(1)} className="button">One Player <i className="icon-menu fa-solid fa-user"></i></button>
           </div>
           <div className="button-row">
-            <button onClick={() => handleState(20)} className="button"> 4 X 5</button>
-            <button onClick={() => handleState(30)} className="button"> 5 X 6</button>
+            <button onClick={() => handlePlayer(2)} className="button">Two Players <i className="icon-menu fa-solid fa-user-group"></i></button>
           </div>
         </div>
       )
     }
     return (
-      <div className='game-menu'>
-        <Game type={game} handleState={handleState} />
-      </div>
+      startGame()
     )
   }
-  const handleState = (state) => {
-    setGame(state)
-  }
 
+  const handleState = state => setGame(state)
+
+  const handlePlayer = num => setPlayer(num)
   return (
     <>
-      {startGame()}
+      {numberOfPlayers()}
       <Footer />
     </>
   )
--- src/components/memory.jsx
+++ src/components/memory.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import arraysGame from "../data/cards.jsx"
 import confetti from 'canvas-confetti'
 
-const Memory = ({ type, handleState }) => {
+const Memory = ({ type, handleState, handlePlayer, player }) => {
     const [typeGame, typeGameState] = useState(type)
     const [board, setBoard] = useState("")
     const [firstCard, setFirstCard] = useState("")
@@ -12,6 +12,12 @@ const Memory = ({ type, handleState }) => {
     const [flippedCard, setFlippedCard] = useState([])
     const [countError, setCountError] = useState(0)
     const [win, setWin] = useState(false)
+
+    // Player state
+    const [playerOne, setPlayerOne] = useState(0)
+    const [playerTwo, setPlayerTwo] = useState(0)
+    const [turn, setTurn] = useState(2)
+
 
     useEffect(() => {
         const manyElements = typeGame / 2
@@ -35,6 +41,7 @@ const Memory = ({ type, handleState }) => {
     }, [])
 
     useEffect(() => {
+        console.log("asd");
         if (secondCard != firstCard && typeof (secondCard) != "string") {
             setTimeout(() => {
                 setCountError(countError + 1)
@@ -47,17 +54,23 @@ const Memory = ({ type, handleState }) => {
 
         } else {
             if (idFirstCard != -1) {
+                setTurn(turn == 1 ? 2 : 1)
                 let newArr = [...flippedCard]
                 newArr.push(idFirstCard)
                 newArr.push(idSecondCard)
                 setFlippedCard(newArr)
+                if (turn == 1) {
+                    setPlayerOne(playerOne + 2)
+                } else { setPlayerTwo(playerTwo + 2) }
                 if (newArr.length == board.length) {
                     confetti()
-                    
                     setWin(true)
-
                 }
+                setTimeout(() => {
+                    setTurn(turn == 1 ? 2 : 1)
+                }, 1000);
             }
+            setTurn(turn == 1 ? 2 : 1)
             setFirstCard("1")
             setSecondCard("")
             setIdFirstCard(-1)
@@ -110,6 +123,13 @@ const Memory = ({ type, handleState }) => {
         setWin(false)
     }
 
+    const winner = () => {
+        if(playerOne > playerTwo) {
+            return "Gano el Jugador 1!!"
+        } 
+        return playerOne == playerTwo ? "Empate" : "Gano el jugador 2!!"
+    }
+
     let cont = 0
     let flipped = false
     return (
@@ -117,53 +137,80 @@ const Memory = ({ type, handleState }) => {
             {
                 board != "" &&
                 <>
-                    <header className='header-memory'>
-                        Memory Game
-                    </header>
-                    <div className='memory'>
-                        {board.map((card) => {
-                            let styleClass = ``;
-                            if (typeGame == 6) {
-                                styleClass = `card card-6`
-                            } else if (typeGame == 12) {
-                                styleClass = `card card-12`
-                            } else if (typeGame == 20) {
-                                styleClass = `card card-20`
-                            } else { styleClass = `card card-30` }
-                            flipped = false
-                            cont += 1
-                            return (
-                                <div className={`${styleClass} ${handleStyle(card[1])} ${isFlipped(card[1])}`} key={card[1]} onClick={() => handleGame(card[0], card[1])}>
-                                    <div className="memory-card-front"></div>
-                                    <div className="memory-card-back">{card[0]}</div>
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <div>
-                        <div className="memory-info">
-                            Number of Errors: {countError}
+                    <div className='game-menu'>
+                        <header className='header-memory'>
+                            Memory Game
+                        </header>
+                        <div className='memory'>
+                            {board.map((card) => {
+                                let styleClass = ``;
+                                if (typeGame == 6) {
+                                    styleClass = `card card-6`
+                                } else if (typeGame == 12) {
+                                    styleClass = `card card-12`
+                                } else if (typeGame == 20) {
+                                    styleClass = `card card-20`
+                                } else { styleClass = `card card-30` }
+                                flipped = false
+                                cont += 1
+                                return (
+                                    <div className={`${styleClass} ${handleStyle(card[1])} ${isFlipped(card[1])}`} key={card[1]} onClick={() => handleGame(card[0], card[1])}>
+                                        <div className="memory-card-front"></div>
+                                        <div className="memory-card-back">{card[0]}</div>
+                                    </div>
+                                )
+                            })}
                         </div>
-                    </div>
-                    <div>
-                        <button className='btn-back' onClick={() => handleState(0)}>
-                            Back
-                        </button>
-                    </div>
-                    {
-                        win &&
-                        <div className='win-modal'>
-                            <div className='txt'>
-                                <div onClick={() => setWin(false)} className='x-modal'>X</div>
-                                <h2>Ganaste!!</h2>
-                                <footer>
-                                    <button onClick={resetGame}>Empezar de nuevo</button>
-                                </footer>
+                        <div>
+                            <div className="memory-info">
+                                Number of Errors: {countError}
                             </div>
                         </div>
-                    }
+                        <div>
+                            <button className='btn-back' onClick={() => {
+                                handleState(0)
+                                handlePlayer(0)
+                            }}>
+                                Back
+                            </button>
+                        </div>
+                        {
+                            win &&
+                            <div className='win-modal'>
+                                <div className='txt'>
+                                    <div onClick={() => setWin(false)} className='x-modal'>X</div>
+                                    <h2>{winner()}</h2>
+                                    <footer>
+                                        <button onClick={resetGame}>Empezar de nuevo</button>
+                                    </footer>
+                                </div>
+                            </div>
+                        }
+                    </div>
                 </>
             }
+            {
+                player == 2 &&
+                (
+                    <div className="aside">
+                        <table className="puntos">
+                            <thead>
+                                <tr>
+                                    <th className={turn == 1 ? "turn" : ""}>Jugador 1</th>
+                                    <th className={turn == 2 ? "turn" : ""}>Jugador 2</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{playerOne}</td>
+                                    <td>{playerTwo}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                )
+            }
+
         </>
     )
 }
